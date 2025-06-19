737e9c9ae244ae1bab3646bf9f2a59e920befb37
Ross Cairns <ross@electricglen.com>
2025-06-19 22:30:51 +0100

    Fixed typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App";
+import App from "./components/app";
 import "./index.css";
 
 const rootElement = document.getElementById("root");
